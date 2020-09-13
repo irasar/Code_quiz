7a9d55a24b83a491573320e19dd118e5b62a5fe0
Sarah Iraurgui <iraurguisj@gmail.com>
2020-09-13 15:18:06 -0400

    questions added with object

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+var question = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["1.Strings", "2.Booleans", "3.Alerts", "4.Numbers"],
+      answer: "Alerts"
+    },
+    {
+      title: "The conditional in an if/else statement is enclosed in ________.:",
+      choices: ["1.Quotes", "2.Curly Braces", "3.Parentheses", "4.Square Brackets"],
+      answer: "Parentheses"
+    },
+        {
+          title: "Arrays in javaScript can be used to store __________.:",
+          choices: ["1.Numbers and strings", "2.Other arrays", "3.Booleans", "4.All of the above"],
+          answer: "All of the above"
+        },
+        {
+          title: "Sting values must be enclosed within:",
+          choices: ["1.Commas", "2.Curly braces", "3.Quotes", "4.Parentheses"],
+          answer: "Quotes"
+        },
+        {
+            title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+            choices: ["1.JavaScript", "2.Terminal/bash", "3.For loops", "4.Console.log"],
+            answer: "Console.log"
+          },
+      ];
+
+
+
+
+
+
